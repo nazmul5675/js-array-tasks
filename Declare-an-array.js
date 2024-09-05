16a8115766b0458abc6d66c949d0842be781865b
nazmul5675 <hridoyn61@gmail.com>
2024-09-05 15:26:04 +0600

    1st task complete

---
+++ Declare-an-array.js
@@ -0,0 +1,11 @@
+// ### 1. Declare an array
+//  1. Declare an array with 5 elements containing fruits
+// 2. console log the 3rd index element
+// 3. change the value of the 2nd index element to jambura
+// 4. console log the final array
+
+let fruits = ['mango', 'banana', 'lichi', 'jackfruit', 'watermelon'];
+console.log(fruits[3]); //3rd index element
+fruits[2] = 'jambura'; //change the value of the 2nd index element to jambura
+console.log(fruits[2]);
+console.log(fruits);//final array
