8c9d30d516334cfcf34f2e5acc20f3b8c6942a72
nazmul5675 <hridoyn61@gmail.com>
2024-09-05 15:35:50 +0600

    array task 2 completed

---
+++ Add-or-remove-elements.js
@@ -0,0 +1,12 @@
+// ### 2. Add or remove elements
+// 1. Declare an array of 3 tourist destinations
+// 2. Add a new tourist destination to your tourist array
+// 3. Add two more to your array
+// 4. Remove the last tourist destination you have added
+// 5. display the final array as output
+
+let touristSpot = ["Cox's bazar", "Bandar ban", 'Shitakundo'];
+touristSpot.push("Mohasthangor");//Add a new tourist destination on array
+console.log(touristSpot);
+touristSpot.pop();
+console.log(touristSpot);
